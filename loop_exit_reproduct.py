2d94756c8bd28ded703e60e7a2ea0012308bc889
Shenxin(Nick) Xu <38663815+nicolasxu-bg@users.noreply.github.com>
2019-07-23 11:28:49 -0400

    Create loop_exit_reproduct.py

---
+++ loop_exit_reproduct.py
@@ -0,0 +1,22 @@
+import gevent
+import time
+
+def idler1():
+	while True:
+		print ('idler1 working...')
+		time.sleep(3)
+		g1.kill()
+
+def idler2():
+	while True:
+		print ('idler2 working...')
+		gevent.sleep(1)
+
+g1 = gevent.Greenlet(idler1)
+g1.start()
+
+# g2 = gevent.Greenlet(idler2)
+# g2.start()
+
+g1.switch() # will trigger the whole thing
+# without g2(the 2nd greenlet), g1.kill() will trigger an LoopExit error.
